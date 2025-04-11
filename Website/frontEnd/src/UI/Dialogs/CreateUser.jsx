6e9bfad27f0e4461f344f29b2788f0d0c87c8c90
Jeon Drew Jaime <89388113+JeonDrewJaime@users.noreply.github.com>
2025-04-11 11:54:30 +0800

    password validation

--- Website/frontEnd/src/UI/Dialogs/CreateUser.jsx
+++ Website/frontEnd/src/UI/Dialogs/CreateUser.jsx
@@ -25,15 +25,16 @@ const validationSchemas = [
   Yup.object({
     password: Yup.string()
       .min(6, 'Password must be at least 6 characters')
-      .matches(/[A-Z]/, 'Password must contain at least one uppercase letter')
-      .matches(/[a-z]/, 'Password must contain at least one lowercase letter')
-      .matches(/[0-9]/, 'Password must contain at least one number')
-      .matches(/[\W_]/, 'Password must contain at least one special character')
+      .matches(
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_])/,
+        'Password must include uppercase, lowercase, number, and special character'
+      )
       .required('Password is required'),
     confirmPassword: Yup.string()
       .oneOf([Yup.ref('password'), null], 'Passwords must match')
       .required('Confirm password is required'),
-  }),
+  })
+  
 ];
 
 
